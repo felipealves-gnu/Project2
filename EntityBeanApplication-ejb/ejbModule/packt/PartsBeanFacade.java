0bd1a9028aa1573a00959dce930a0d5ff4fce6c5
felipe <felipe@felipe-pc>
2013-06-04 20:11:22 -0300

    Entity Bean

---
+++ EntityBeanApplication-ejb/ejbModule/packt/PartsBeanFacade.java
@@ -0,0 +1,23 @@
+package packt;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class PartsBeanFacade extends AbstractFacade<PartsBean> {
+
+    @PersistenceContext(unitName = "EntityBeanApplication-ejbPU")
+    private EntityManager em;
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    public PartsBeanFacade(){
+        super(PartsBean.class);
+    }
+
+
+}
