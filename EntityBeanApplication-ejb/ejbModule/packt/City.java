0801c2e3187b32f4a9213ed52c433c9ca936b971
felipe <felipe@felipe-PC>
2013-06-08 10:54:13 -0300

    new project working.

---
+++ EntityBeanApplication-ejb/ejbModule/packt/City.java
@@ -0,0 +1,64 @@
+package packt;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: City
+ *
+ */
+@Entity
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String country;
+	private long population;
+	
+	public City() {
+		super();
+	}
+	
+	public City(String name, String country, long population) {
+		this.name = name;
+		this.country = country;
+		this.population = population;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public long getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(long population) {
+		this.population = population;
+	}
+	
+	
+}
